e97905ac9cef4fa470322afab1f1b9f758c0ca06
wuchenghui <wuchenghui@wejoydata.com>
2020-05-11 19:37:31 +0800

    fix install_required

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 ```
 pip install didtool
 ```
+or 
+```
+python setup.py bdist_wheel
+cd dist/
+pip install didtool-{version}-py3-none-any.whl
+```
--- setup.py
+++ setup.py
@@ -13,10 +13,18 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dustless/didtool",
     packages=setuptools.find_packages(),
+    install_requires=[
+        "seaborn>=0.10.1",
+        "scikit-learn>=0.21.3",
+        "lightgbm>=2.3.1",
+        "sklearn2pmml==0.52.1",
+        "pandas",
+        "numpy",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
