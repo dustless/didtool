e9c82b4edcef1b09f13b65931b8a063bcac0e0d5
yangfan30 <yangfan30@wejoydata.com>
2020-06-08 12:52:25 +0800

    format change

---
+++ didtool/logger.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Project -> File   ：didtool -> logger
+@IDE    ：PyCharm
+@Author ：Yangfan
+@Date   ：2020/6/8 12:39
+@Desc   ：
+=================================================='''
+import sys
+class Logger(object):
+	def __init__(self, file_name="Default.log"):
+		self.terminal = sys.stdout
+		self.log = open(file_name, "w",encoding='utf-8')
+
+	def write(self, message):
+		self.terminal.write(message)
+		self.log.write(message)
+		# 每次写入后刷新到文件中，防止程序意外结束
+		self.flush()
+
+	def flush(self):
+		self.log.flush()
