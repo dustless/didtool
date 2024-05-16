2f2457415e30d8104a166298369f6bb529af6251
wuchenghui <wuchenghui@baidu.com>
2024-05-16 09:38:09 +0800

    release 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="didtool",
-    version="0.1.1",
+    version="0.1.2",
     author="dustless",
     author_email="wuchenghui927@126.com",
     description="Tool set for feature engineering & modeling",
